0f5bdd1ab693958c9cd7f53b0c9d5657f0b4a97c
Kostiantyn Perun <bbd.ids@gmail.com>
2021-01-13 14:31:44 +0200

    Refactored class and functions

--- MorskoyBoy.py
+++ MorskoyBoy.py
@@ -70,10 +70,10 @@ class Grid:
                              2+self.offset, upper_margin - block_size//2 - font_size))
 
 
-class ShipsOnGrid:
-    def __init__(self):
-        self.available_blocks = {(a, b) for a in range(1, 11)
-                                 for b in range(1, 11)}
+class AutoShips:
+    def __init__(self, offset):
+        self.offset = offset
+        self.available_blocks = {(x, y) for x in range(1+self.offset, 11+self.offset) for y in range(1, 11)}
         self.ships_set = set()
         self.ships = self.populate_grid()
 
@@ -89,17 +89,17 @@ class ShipsOnGrid:
         for _ in range(number_of_blocks):
             ship_coordinates.append((x, y))
             if not x_or_y:
-                str_rev, x = self.add_block_to_ship(
+                str_rev, x = self.get_new_block_for_ship(
                     x, str_rev, x_or_y, ship_coordinates)
             else:
-                str_rev, y = self.add_block_to_ship(
+                str_rev, y = self.get_new_block_for_ship(
                     y, str_rev, x_or_y, ship_coordinates)
         if self.is_ship_valid(ship_coordinates):
             return ship_coordinates
         return self.create_ship(number_of_blocks, available_blocks)
 
-    def add_block_to_ship(self, coor, str_rev, x_or_y, ship_coordinates):
-        if (coor <= 1 and str_rev == -1) or (coor >= 10 and str_rev == 1):
+    def get_new_block_for_ship(self, coor, str_rev, x_or_y, ship_coordinates):
+        if (coor <= 1-self.offset*(x_or_y-1) and str_rev == -1) or (coor >= 10-self.offset*(x_or_y-1) and str_rev == 1):
             str_rev *= -1
             return str_rev, ship_coordinates[0][x_or_y] + str_rev
         else:
@@ -110,8 +110,7 @@ class ShipsOnGrid:
         return ship.issubset(self.available_blocks)
 
     def add_new_ship_to_set(self, new_ship):
-        for elem in new_ship:
-            self.ships_set.add(elem)
+        self.ships_set.update(new_ship)
 
     def update_available_blocks_for_creating_ships(self, new_ship):
         for elem in new_ship:
@@ -137,18 +136,14 @@ def draw_ships(ships_coordinates_list):
         ship = sorted(elem)
         x_start = ship[0][0]
         y_start = ship[0][1]
-        # Vert
+        # Horizontal and 1block ships
+        ship_width = block_size * len(ship)
+        ship_height = block_size
+        # Vertical ships
         if len(ship) > 1 and ship[0][0] == ship[1][0]:
-            ship_width = block_size
-            ship_height = block_size * len(ship)
-        # Hor and 1block
-        else:
-            ship_width = block_size * len(ship)
-            ship_height = block_size
+            ship_width, ship_height = ship_height, ship_width
         x = block_size * (x_start - 1) + left_margin
         y = block_size * (y_start - 1) + upper_margin
-        if ships_coordinates_list == human.ships:
-            x += 15 * block_size
         pygame.draw.rect(
             screen, BLACK, ((x, y), (ship_width, ship_height)), width=block_size//10)
 
@@ -160,70 +155,52 @@ def computer_shoots(set_to_shoot_from):
     return check_hit_or_miss(computer_fired_block, human_ships_working, True)
 
 
-def check_hit_or_miss(fired_block, opponents_ships_list, computer_turn, diagonal_only=True):
+def check_hit_or_miss(fired_block, opponents_ships_list, computer_turn, opponents_ships_list_original_copy, opponents_ships_set):
     for elem in opponents_ships_list:
+        diagonal_only = True
         if fired_block in elem:
-            update_dotted_and_hit_sets(
-                fired_block, computer_turn, diagonal_only=True)
             ind = opponents_ships_list.index(elem)
-
             if len(elem) == 1:
-                update_dotted_and_hit_sets(
-                    fired_block, computer_turn, diagonal_only=False)
-
+                diagonal_only = False
+            update_dotted_and_hit_sets(fired_block, computer_turn, diagonal_only)
             elem.remove(fired_block)
-
+            opponents_ships_set.discard(fired_block)
             if computer_turn:
                 last_hits_list.append(fired_block)
-                human.ships_set.discard(fired_block)
                 update_around_last_computer_hit(fired_block)
-            else:
-                computer.ships_set.discard(fired_block)
-
             if not elem:
-                draw_destroyed_ships(ind, opponents_ships_list, computer_turn)
+                update_destroyed_ships(ind, computer_turn, opponents_ships_list_original_copy)
                 if computer_turn:
                     last_hits_list.clear()
                     around_last_computer_hit_set.clear()
                 else:
                     destroyed_ships_list.append(computer.ships[ind])
-
             return True
-    put_dot_on_missed_block(fired_block, computer_turn)
+    add_missed_block_to_dotted_set(fired_block, computer_turn)
     if computer_turn:
         update_around_last_computer_hit(fired_block, False)
     return False
 
 
-def put_dot_on_missed_block(fired_block, computer_turn=False):
-    if not computer_turn:
-        dotted_set.add(fired_block)
-    else:
-        dotted_set.add((fired_block[0] + 15, fired_block[1]))
-        dotted_set_for_computer_not_to_shoot.add(fired_block)
+def add_missed_block_to_dotted_set(fired_block):
+    dotted_set.add(fired_block)
+    dotted_set_for_computer_not_to_shoot.add(fired_block)
 
 
-def draw_destroyed_ships(ind, opponents_ships_list, computer_turn, diagonal_only=False):
-    if opponents_ships_list == computer_ships_working:
-        ships_list = computer.ships
-    elif opponents_ships_list == human_ships_working:
-        ships_list = human.ships
-    ship = sorted(ships_list[ind])
+def update_destroyed_ships(ind, computer_turn, opponents_ships_list_original_copy):
+    ship = sorted(opponents_ships_list_original_copy[ind])
     for i in range(-1, 1):
-        update_dotted_and_hit_sets(ship[i], computer_turn, diagonal_only)
+        update_dotted_and_hit_sets(ship[i], computer_turn, False)
 
 
 def update_around_last_computer_hit(fired_block, computer_hits=True):
     global around_last_computer_hit_set, computer_available_to_fire_set
     if computer_hits and fired_block in around_last_computer_hit_set:
         around_last_computer_hit_set = computer_hits_twice()
-
     elif computer_hits and fired_block not in around_last_computer_hit_set:
         computer_first_hit(fired_block)
-
     elif not computer_hits:
         around_last_computer_hit_set.discard(fired_block)
-
     around_last_computer_hit_set -= dotted_set_for_computer_not_to_shoot
     around_last_computer_hit_set -= hit_blocks_for_computer_not_to_shoot
     computer_available_to_fire_set -= around_last_computer_hit_set
@@ -250,19 +227,16 @@ def computer_hits_twice():
         x2 = last_hits_list[i+1][0]
         y1 = last_hits_list[i][1]
         y2 = last_hits_list[i+1][1]
-
         if x1 == x2:
             if y1 > 1:
                 new_around_last_hit_set.add((x1, y1 - 1))
             if y2 < 10:
                 new_around_last_hit_set.add((x1, y2 + 1))
-
         elif y1 == y2:
             if 1 < x1:
                 new_around_last_hit_set.add((x1 - 1, y1))
             if x2 < 10:
                 new_around_last_hit_set.add((x2 + 1, y1))
-
     return new_around_last_hit_set
 
 
@@ -309,8 +283,8 @@ def draw_hit_blocks(hit_blocks):
                          (x1+block_size, y1), block_size//6)
 
 
-computer = ShipsOnGrid()
-human = ShipsOnGrid()
+computer = AutoShips(0)
+human = AutoShips(15)
 computer_ships_working = copy.deepcopy(computer.ships)
 human_ships_working = copy.deepcopy(human.ships)
 
